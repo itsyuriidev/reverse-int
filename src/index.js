c1bf0f5eb6f92391a5b1d18d5f9b8d549250c335
itsyura <itsyura19@gmail.com>
2022-01-10 22:52:17 +0200

    feat: add solve reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
-}
+    let num = String(n);
+    let str = '';
+    for (let i = num.length; i >= 0; i--){
+      if(num[i] >= 0) {
+        str += num[i];
+      }
+    }return str;
+  };
